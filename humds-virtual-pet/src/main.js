e44be8621a9b5de308d83e0a9ca887c0ed65d467
humd1 <humdb@nycstudents.net>
2025-11-05 11:51:48 -0500

    k

--- humds-virtual-pet/src/main.js
+++ humds-virtual-pet/src/main.js
@@ -4,3 +4,23 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
 
+const lightBtn = document.getElementById("light");
+const darkBtn = document.getElementById("dark");
+const warmBtn = document.getElementById("warm");
+
+const body = document.body;
+
+lightBtn.addEventListener("click", () => {
+  body.classList.remove("darkmode");
+  body.classList.add("lightmode");
+});
+
+darkBtn.addEventListener("click", () => {
+  body.classList.remove("lightmode");
+  body.classList.add("darkmode");
+});
+
+warmBtn.addEventListener("click", () => {
+  element.classList.remove(...element.classList);
+  body.classList.add("warm");
+});
