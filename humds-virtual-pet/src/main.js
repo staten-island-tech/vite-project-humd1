98e464f2443fe6e31af88f076c085ae016a2eb4e
humd1 <humdb@nycstudents.net>
2025-11-17 11:40:50 -0500

    havent commited in a while

--- humds-virtual-pet/src/main.js
+++ humds-virtual-pet/src/main.js
@@ -3,24 +3,46 @@ import javascriptLogo from './javascript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 
-
-const lightBtn = document.getElementById("light");
-const darkBtn = document.getElementById("dark");
-const warmBtn = document.getElementById("warm");
+const pets = [
+  {
+    name: "Placeholder",
+    hunger: 10,
+    energy: 10,
+    level: 10,
+    happiness: 10,
+    unlocked: true
+  }
+]
 
 const body = document.body;
 
-lightBtn.addEventListener("click", () => {
-  body.classList.remove("darkmode");
-  body.classList.add("lightmode");
-});
+document.querySelectorAll(".colorselector").forEach(btn =>
+  btn.addEventListener("click", () => body.className = btn.id)
+);
 
-darkBtn.addEventListener("click", () => {
-  body.classList.remove("lightmode");
-  body.classList.add("darkmode");
-});
 
-warmBtn.addEventListener("click", () => {
-  element.classList.remove(...element.classList);
-  body.classList.add("warm");
-});
+document.querySelectorAll(".textselector").forEach(btn =>
+  btn.addEventListener("click", () => body.className = btn.id)
+);
+
+
+const petsDiv = document.querySelector(".pets");
+
+function insertpet(pet){
+  petsDiv.insertAdjacentHTML
+  ("afterbegin")
+    `<div class="pet"></div>
+    <h2>${pet.name}</h2>
+    <p><span>Hunger: ${pet.hunger}</span></p>
+    <p><span>Energy: ${pet.energy}</span></p>
+    <p><span>Level: ${pet.level}</span></p>
+    <p><span>Happiness: ${pet.happiness}</span></p>
+    `
+}
+
+const viewpets= document.querySelector(".viewpets");
+
+viewpets.addEventListener("click", () => {
+  pets.forEach(insertpet);
+}
+);
